a8a1e7bd6603eca715de71c6846f2eb2db384667
Sarah Shahzad <f24605008@nutech.edu.pk>
2025-07-26 13:57:36 +0500

    new commit

---
+++ arithmaticOp.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b) #return floating value
+print("Remainder:", a % b)
+print("Exponent:", a ** b) #b is power of a 
+print("Floor Division:", a // b) #return integer value
