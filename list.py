850829c81cbe2308217e89729e93ca91e858ddba
Sarah Shahzad <f24605008@nutech.edu.pk>
2025-07-29 04:38:50 +0500

    new

--- list.py
+++ list.py
@@ -1,2 +1,9 @@
 #Section: 5
 #list data type
+var_list= ['Sarah', 18]
+#has ordered sequence(indexing)
+l_one = [1,2,3,4]
+l_two = [1,2,["three"],4]
+l_three = list((1,2,3,4))
+print(l_one[3])
+print(l_one[0:3]) #3rd element included
