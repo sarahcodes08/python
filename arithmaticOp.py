20278bae75be5ce5f9b59e3af282e4bd57317ee9
Sarah Shahzad <f24605008@nutech.edu.pk>
2025-08-08 19:38:38 +0500

    new:

--- arithmaticOp.py
+++ arithmaticOp.py
@@ -16,3 +16,44 @@ print("Order of precedence:", a+b/a*b)
 #2. Exponentiation
 #3. Multiplication and Division 
 #4. Addition and Subtraction
+
+	# 45361231321 = 10 no number
+	# 23test = 10 no num at start
+	#$%&* = 10 no special char
+	# print = 10 no name after build in fun
+	# n ame=10 no space in between
+	 
+	# var_bio_result = 10
+	 
+	# b = 20
+	# print(a)
+	# print(b)
+	#
+	# a,b = 20,10
+	# print(a,b)
+	 
+a = 13
+b = 6
+	 
+print(a+b) #Addition
+print(a-b) #Subraction
+print(a*b) #Multiplication
+print(a**b) #Exponent
+print(a/b) #Division, this will return floating value
+print(a//b) #Double Division
+print(a%b) #Modulus
+	 
+print(a+(b-a)+b) #Parenthesis
+	 
+print(a+b/a*b)
+	#P arenthesis
+	#E xponent
+	#D ivision
+	#M ultiplication
+	#A ddition
+	#S ubraction
+	 
+print(3+2-5)
+print((3-2)*3**2*a%b*a/b/a%b)
+print("Hello's world")
+ 
---
+++ bool.py
@@ -0,0 +1,4 @@
+#Boolean Data Type
+#two conditions; true and false
+#Comparison Operators
+# ==, !=, >, <, >=, <=
