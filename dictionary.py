1df186331f5062172dd4822334b949334323a769
Sarah Shahzad <f24605008@nutech.edu.pk>
2025-08-12 06:17:36 +0500

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -8,3 +8,19 @@ dict_a = {
 print(dict_a)
 print(dict_a['key1'])
 print(dict_a.get('key5'))
+
+car = {
+  'brand': 'Ford',
+  'model': 'Mustang',
+  'year': [1964, 1965, 1966],
+  'color': ('red', 'blue', 'green'),
+  'docs': {'original':'yes', 'duplicate':'no'}
+}
+print(car['brand'])
+print(car['model'])
+print(car['year'])
+print(car['year'][0])
+print(car['color'])
+print(car['color'][1])
+print(car['docs'])
+print(car['docs']['original'])
